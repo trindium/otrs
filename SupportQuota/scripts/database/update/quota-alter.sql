67b3634feb3b3ee45a2671d5e4c1b9b56592aa28
Deny Dias <deny@macpress.com.br>
2014-07-14 19:33:32 -0300

    SupportQuota: remove unnecessary files.

--- SupportQuota/scripts/database/update/quota-alter.sql
+++
@@ -1,4 +0,0 @@
-# ----------------------------------------------------------
-#  alter table customer_company
-# ----------------------------------------------------------
-ALTER TABLE customer_company ADD quota SMALLINT;
